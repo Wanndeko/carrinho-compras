8f719820de1a2f3106f548a89de5e958d1d17f28
Wanndeko <camposwanderley418@gmail.com>
2023-05-25 12:43:39 -0300

    feat: header final css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,19 @@
-export function Header() {}
+import CartButton from '../CartButton/CartButton'
+import SearchBar from '../SearchBar/SearchBar'
+
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <div className="container">
+          <SearchBar />
+          <CartButton />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BsSearch } from 'react-icons/bs'
+import './SearchBar.css'
+
+function SearchBar() {
+  const [searchValue, setSearchValue] = useState('')
+
+  return (
+    <form className="search-bar">
+      <input
+        type="search"
+        value={searchValue}
+        placeholder="Buscar Produtos"
+        className="search__input"
+        required
+        onChange={({ target }) => setSearchValue(target.value)}
+      />
+
+      <button type="submit" className="search__button">
+        <BsSearch />
+      </button>
+    </form>
+  )
+}
+
+export default SearchBar
